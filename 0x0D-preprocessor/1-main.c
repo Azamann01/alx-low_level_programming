91e523409a278f8fc01efdaba45a515312f97661
adeniyi tope wilson <adeniyitopewilson@gmail.com>
2023-10-11 01:46:52 -0700

    preprocessor

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,16 @@
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
+}
